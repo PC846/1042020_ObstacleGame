770503d47383598062a20cdd53eb26e08d962bc0
PC846 <73675772+PC846@users.noreply.github.com>
2020-10-29 21:04:35 -0400

    Game Assets

---
+++ Assets/Player_Collision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Player_Collision : MonoBehaviour
+{
+
+    public Player_Move movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
+       
+
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            Debug.Log(collisionInfo.collider.name);
+            
+        }
+    }
+}
---
+++ Assets/Player_Move.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    // Reference to Rigidbody
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+
+    public float sidewaysForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+    }
+
+    // Marked this as "Fixed"Update because we
+    // are using it to mess with physics using Unity.
+
+    // Time.deltaTime makes it so that it looks the same
+    // accross all computers. It takes how much time gathered
+    // from last frame.
+
+   
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce* Time.deltaTime); // adds a forward force 
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-(sidewaysForce) * Time.deltaTime, 0, 0);
+        }
+
+    }
+}
